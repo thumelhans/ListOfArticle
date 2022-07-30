11a4223e9074d3bae509e09175aa096e3adc59d4
Tristan <tristan.humelhans57@gmail.com>
2022-07-30 08:46:12 +0200

    **** Exercice: JS ajouter/supprimer un article dans un tableau HTML v0.1 ****
    Ajout:
    
    	*** HTML ***
    
    	- Création du header
    	- Création du footer
    	- Création du conteneur tableau
    	- Création du formulaire
    
    	*** CSS ***
    
    	- Configuration du style principal

---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+
+App gérant l'ajout, la modification et la suppression d'une liste d'article
+
+Créer un article ---> ajouter une ligne au tableau
+modifier un article ---> modifier l'élément sélectionné
+supprimer un article ---> supprimer la ligne du tableau
+
+colonne ID = list index
+colonne nom = name
+colonne quantité = quantity
+colonne prix = price
+colonne total = price * quantity
+
+
+
+*/
